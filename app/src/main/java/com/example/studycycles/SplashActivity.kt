ea201f561739f7f43293ccc92328bba4cee6b403
Kislatron <kislatron@gmail.com>
2020-06-17 11:48:36 -0500

    Splash

--- app/src/main/java/com/example/studycycles/SplashActivity.kt
+++ app/src/main/java/com/example/studycycles/SplashActivity.kt
@@ -1,12 +1,22 @@
 package com.example.studycycles
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_splash.*
 
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        iv_jelly.alpha = 0f
+        iv_jelly.animate().setDuration(4000).alpha(1f).withEndAction{
+            val i = Intent(this, MainActivity::class.java)
+            startActivity(i)
+            overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out)
+            finish()
+        }
     }
 }
